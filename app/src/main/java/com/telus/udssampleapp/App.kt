2c47860783a2ac64a148b6f60b894dcdd64f8564
Jian Liew <jian@steamclock.com>
2022-09-28 14:30:20 -0700

    removed UDSLibarary as how tokens are generated is changing

--- app/src/main/java/com/telus/udssampleapp/App.kt
+++ app/src/main/java/com/telus/udssampleapp/App.kt
@@ -1,16 +1,12 @@
 package com.telus.udssampleapp
 
 import android.app.Application
-import com.telus.udsnative.UDSLibrary
 
 class App: Application() {
 
 
     override fun onCreate() {
         super.onCreate()
-
-        //configure UDS Library with them and pallet tokens
-        UDSLibrary.configure("{}", "{}")
     }
 
 }
