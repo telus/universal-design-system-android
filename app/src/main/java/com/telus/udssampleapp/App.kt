e7c1b7ea42299a0e9355937097c18504a2aad497
munl <munjianliew@gmail.com>
2022-09-28 14:34:32 -0700

    [#4] initial project creation (#11)
    * [#4] initial project creation
    
    * remove .gradle files
    
    * removed local properties
    
    * update gitignore
    
    * removed UDSLibarary as how tokens are generated is changing
    
    * removed pallet and theme tokens
    
    Co-authored-by: Jian Liew <jian@steamclock.com>

---
+++ app/src/main/java/com/telus/udssampleapp/App.kt
@@ -0,0 +1,12 @@
+package com.telus.udssampleapp
+
+import android.app.Application
+
+class App: Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/com/telus/udssampleapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.telus.udssampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
