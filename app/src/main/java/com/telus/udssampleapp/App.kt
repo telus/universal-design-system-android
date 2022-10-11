3d72fdd12d5eb5a5320d1d7f5452a867b04d547d
Jenn Cooper <cooper.jennl@gmail.com>
2022-10-11 13:04:05 -0700

    Load json from file

--- app/src/main/java/com/telus/udssampleapp/App.kt
+++ app/src/main/java/com/telus/udssampleapp/App.kt
@@ -1,12 +1,21 @@
 package com.telus.udssampleapp
 
 import android.app.Application
+import android.content.res.AssetManager
+import com.telus.udsnative.ThemeResolver
 
 class App: Application() {
-
-
     override fun onCreate() {
+        val jsonString = assets.readAssetsFile("testData/${TestTheme.allium}")
+        ThemeResolver.setup(jsonString = jsonString)
+
         super.onCreate()
     }
-
 }
+
+object TestTheme {
+    val allium: String = "AlliumTheme.json"
+    val koodo: String = "KoodoTheme.json"
+}
+
+fun AssetManager.readAssetsFile(fileName : String): String = open(fileName).bufferedReader().use { it.readText() }
---
+++ udsnative/src/main/java/com/telus/udsnative/ThemeResolver.kt
@@ -0,0 +1,7 @@
+package com.telus.udsnative
+
+object ThemeResolver {
+    fun setup(jsonString: String) {
+        val string = jsonString
+    }
+}
