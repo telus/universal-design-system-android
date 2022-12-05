beb1618e2629c10c5fe2b642b38c64223d42b9a0
Jian Liew <jian@steamclock.com>
2022-12-05 13:47:19 -0800

    updated card component for background

--- udsnative/src/main/java/com/telus/udsnative/components/card/Card.kt
+++ udsnative/src/main/java/com/telus/udsnative/components/card/Card.kt
@@ -1,5 +1,6 @@
 package com.telus.udsnative.components.card
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -7,14 +8,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.telus.udsnative.ThemeResolver
 import com.telus.udsnative.components.card.models.CustomPadding
@@ -64,10 +59,10 @@ fun Card(
         )
     }
 
-    androidx.compose.material.Card(
-        modifier = cardModifier,
-        shape = RoundedCornerShape(tokens.borderRadius),
-        backgroundColor = tokens.backgroundColor.color
+    Box(
+        modifier = cardModifier
+            .clip(RoundedCornerShape(tokens.borderRadius))
+            .background(tokens.backgroundColor.color)
     ) {
         Column (
             modifier = Modifier
